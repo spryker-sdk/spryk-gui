6c48c4931d14e2de4daa91e1aa29a46b2bd9c699
Anton Sakharov <anton.sakharov@spryker.com>
2021-09-22 20:48:11 +0300

    TE-9771: Refactor into use of face test application

--- ci/stores.php
+++ ci/stores.php
@@ -2,7 +2,24 @@
 
 $stores = [];
 
-$stores['DE']['queuePools']['synchronizationPool'] = ['DE-connection'];
+$stores['DE'] = [
+    'queuePools' => [
+        'synchronizationPool' => [
+            'DE-connection'
+        ]
+    ],
+    'locales' => [
+        'de' => 'de_DE',
+    ],
+    'countries' => [
+        'DE',
+        'AT',
+        'NO',
+        'CH',
+        'ES',
+        'GB'
+    ],
+];
 $stores['AT']['queuePools']['synchronizationPool'] = ['DE-connection'];
 $stores['US']['queuePools']['synchronizationPool'] = ['DE-connection'];
 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,14 @@
 <?php
+if (!defined('MODULE_ROOT_DIR')) {
+    define('MODULE_ROOT_DIR', realpath(__DIR__ . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPARATOR);
+}
 
 if (!defined('APPLICATION_ROOT_DIR')) {
-    define('APPLICATION_ROOT_DIR', realpath(__DIR__ . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPARATOR);
+    define('APPLICATION_ROOT_DIR', realpath(__DIR__ . DIRECTORY_SEPARATOR ) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR);
 }
 
 if (!defined('APPLICATION_VENDOR_DIR')) {
-    define('APPLICATION_VENDOR_DIR', APPLICATION_ROOT_DIR . 'vendor' . DIRECTORY_SEPARATOR);
+    define('APPLICATION_VENDOR_DIR', realpath(__DIR__ . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR);
 }
 
 if (!defined('APPLICATION_STORE')) {
@@ -17,14 +20,22 @@ if (!defined('APPLICATION_ENV')) {
 }
 
 // Copy config files
-$configTargetDirectory = APPLICATION_ROOT_DIR . 'config' . DIRECTORY_SEPARATOR . 'Shared'  . DIRECTORY_SEPARATOR;
-if (!is_dir($configTargetDirectory)) {
-    mkdir($configTargetDirectory, 0777, true);
+$configSharedTargetDirectory = APPLICATION_ROOT_DIR . 'config' . DIRECTORY_SEPARATOR . 'Shared'  . DIRECTORY_SEPARATOR;
+if (!is_dir($configSharedTargetDirectory)) {
+    mkdir($configSharedTargetDirectory, 0777, true);
 }
 
-$configSourceDirectory = APPLICATION_ROOT_DIR . 'ci' . DIRECTORY_SEPARATOR;
-copy($configSourceDirectory . 'config_local.php', $configTargetDirectory . 'config_local.php');
-copy($configSourceDirectory . 'stores.php', $configTargetDirectory . 'stores.php');
+$configZedTargetDirectory = APPLICATION_ROOT_DIR . 'config' . DIRECTORY_SEPARATOR . 'Zed'  . DIRECTORY_SEPARATOR;
+if (!is_dir($configZedTargetDirectory)) {
+    mkdir($configZedTargetDirectory, 0777, true);
+}
+
+$configSourceDirectory = MODULE_ROOT_DIR . 'ci' . DIRECTORY_SEPARATOR;
+copy($configSourceDirectory . 'config_default.php', $configSharedTargetDirectory . 'config_default.php');
+copy($configSourceDirectory . 'default_store.php', $configSharedTargetDirectory . 'default_store.php');
+
+copy($configSourceDirectory . 'stores.php', $configSharedTargetDirectory . 'stores.php');
+copy(MODULE_ROOT_DIR . 'src/SprykerSdk/Zed/SprykGui/Communication/navigation.xml', $configZedTargetDirectory . 'navigation.xml');
 
 $config = \Spryker\Shared\Config\Config::getInstance();
 $config->init();
